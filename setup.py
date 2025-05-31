4db280aae524acd9fd35a879fc707cb77dc9c4ab
Shashank Sahay <sahayshashank28@gmail.com>
2025-05-31 18:41:45 +0000

    Rename pyLog to LoggerPython
    This commit has the following changes:
    1. Links rename to LoggerPython
    2. package rename to loggerpython

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pyLog',
+    name='loggerpython',
     version='0.1.0',
     author='Mainframe Matrix',
     author_email='mainframematrix01@gmail.com',
     description='A Simple Python Logging Framework.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/mainframematrix/pyLog',
+    url='https://github.com/mainframematrix/LoggerPython',
     packages=find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
